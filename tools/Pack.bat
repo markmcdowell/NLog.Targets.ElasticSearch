d3d696acb9ba712277743ddde61e03155c174143
Mark <mark.mcdowell@me.com>
2015-02-01 21:21:04 +0000

    Incrementing version number

--- tools/Pack.bat
+++ tools/Pack.bat
@@ -1,5 +1,5 @@
 @ECHO OFF 
-SET version=0.9.4.0
+SET version=0.9.5.0
 SET out=.
 "C:\Program Files (x86)\NuGet\nuget.exe" pack ..\src\NLog.Targets.ElasticSearch\NLog.Targets.ElasticSearch.csproj -Symbols -Prop Configuration=Release -OutputDirectory %out% -Version %version%
 pause
