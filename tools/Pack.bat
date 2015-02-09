c4d10afc4f6d0d56266fdc700bcbcf97c2493126
Mark <mark.mcdowell@me.com>
2015-02-09 22:29:21 +0000

    Appveyor packing build instead

--- tools/Pack.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF 
-SET version=1.0.0.0
-SET out=.
-"C:\Program Files (x86)\NuGet\nuget.exe" pack ..\src\NLog.Targets.ElasticSearch\NLog.Targets.ElasticSearch.csproj -Symbols -Prop Configuration=Release -OutputDirectory %out% -Version %version%
-pause
