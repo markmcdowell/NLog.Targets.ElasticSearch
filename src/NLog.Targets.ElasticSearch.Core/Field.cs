8c270fcf69f060d126b1a2be20839349275b11ca
peske <peske.nis@gmail.com>
2016-04-12 14:12:52 +0200

    .NET Platform 5.4 (aka .NET Core) version created. The change in the original code is trivial (few lines in only one file) and does not affect existing net45 version. Conditional compiling is used, so that the orignal version does not "see" any change at all.
    The new project is created (NLog.Targets.ElasticSearch.Core) and it builds both versions (dotnet5.4 and net45). The only change made in the original project (NLog.Targets.ElasticSearch) is that compilation symbol "NET45" is added.
    
    All code files (*.cs) are identitcal in both projects.

---
+++ src/NLog.Targets.ElasticSearch.Core/Field.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NLog.Config;
+using NLog.Layouts;
+
+namespace NLog.Targets.ElasticSearch
+{
+    [NLogConfigurationItem]
+    public class Field
+    {
+        public Field()
+        {
+            LayoutType = typeof (string);
+        }
+
+        [RequiredParameter]
+        public string Name { get; set; }
+
+        [RequiredParameter]
+        public Layout Layout { get; set; }
+
+        public Type LayoutType { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, LayoutType: {LayoutType}, Layout: {Layout}";
+        }
+    }
+}
---
+++ src/NLog.Targets.ElasticSearch.Core/IElasticSearchTarget.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Elasticsearch.Net;
+using NLog.Layouts;
+
+namespace NLog.Targets.ElasticSearch
+{
+    public interface IElasticSearchTarget
+    {
+        /// <summary>
+        /// Gets or sets a connection string name to retrieve the Uri from.
+        /// 
+        /// Use as an alternative to Uri
+        /// </summary>
+        string ConnectionStringName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the elasticsearch uri, can be multiple comma separated.
+        /// </summary>
+        string Uri { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the elasticsearch index to write to.
+        /// </summary>
+        Layout Index { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether to include all properties of the log event in the document
+        /// </summary>
+        bool IncludeAllProperties { get; set; }
+
+        /// <summary>
+        /// Gets or sets a comma separated list of excluded properties when setting <see cref="IncludeAllProperties"/>
+        /// </summary>
+        string ExcludedProperties { get; set; }
+
+        /// <summary>
+        /// Gets or sets the document type for the elasticsearch index.
+        /// </summary>
+        Layout DocumentType { get; set; }
+
+        /// <summary>
+        /// Gets or sets a list of additional fields to add to the elasticsearch document.
+        /// </summary>
+        IList<Field> Fields { get; set; }
+
+        /// <summary>
+        /// Gets or sets an alertnative serializer for the elasticsearch client to use.
+        /// </summary>
+        IElasticsearchSerializer ElasticsearchSerializer { get; set; }
+
+        /// <summary>
+        /// Gets or sets if exceptions will be rethrown.
+        /// 
+        /// Set it to true if ElasticSearchTarget target is used within FallbackGroup target (https://github.com/NLog/NLog/wiki/FallbackGroup-target).
+        /// </summary>
+        bool ThrowExceptions { get; set; } 
+    }
+}
