5d4d0cf6e65581684b579304e77641ce107e3c1b
Sam Cook <github@sam.net.nz>
2019-09-17 18:20:30 +0100

    Add test to demonstrate logging failure for log events that have exceptions that can't be serialised due to a property throwing an exception when fetched.

--- src/NLog.Targets.ElasticSearch.Tests/IntegrationTests.cs
+++ src/NLog.Targets.ElasticSearch.Tests/IntegrationTests.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using NLog.Common;
 using NLog.Config;
+using NLog.Layouts;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace NLog.Targets.ElasticSearch.Tests
 {
     public class IntegrationTests
     {
-        [Fact(Skip ="Integration")]
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public IntegrationTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        private class ExceptionWithPropertiesThatThrow : Exception
+        {
+            public object ThisPropertyThrowsOnGet => throw new ObjectDisposedException("DisposedObject");
+        }
+
+        [Theory(Skip = "Integration")]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void ExceptionSerializationTest(bool hasExceptionFieldLayout)
+        {
+            using (var testOutputTextWriter = new TestOutputTextWriter(testOutputHelper))
+            {
+                InternalLogger.LogWriter = testOutputTextWriter;
+                InternalLogger.LogLevel = LogLevel.Warn;
+
+                var elasticTarget = new ElasticSearchTarget();
+
+                if (hasExceptionFieldLayout)
+                {
+                    elasticTarget.Fields.Add(new Field
+                    {
+                        Name = "exception",
+                        Layout = Layout.FromString("${exception:format=toString,Data:maxInnerExceptionLevel=10}"),
+                        LayoutType = typeof(string)
+                    });
+                }
+
+                var rule = new LoggingRule("*", LogLevel.Info, elasticTarget);
+
+                var config = new LoggingConfiguration();
+                config.LoggingRules.Add(rule);
+
+                LogManager.Configuration = config;
+
+                var logger = LogManager.GetLogger("Example");
+
+                logger.Error(new ExceptionWithPropertiesThatThrow(), "Boom");
+
+                LogManager.Flush();
+            }
+        }
+
+        [Fact(Skip = "Integration")]
         public void SimpleLogTest()
         {
             var elasticTarget = new ElasticSearchTarget();
---
+++ src/NLog.Targets.ElasticSearch.Tests/TestOutputTextWriter.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace NLog.Targets.ElasticSearch.Tests
+{
+    public class TestOutputTextWriter : TextWriter
+    {
+        private StringBuilder stringBuilder;
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public TestOutputTextWriter(ITestOutputHelper testOutputHelper)
+        {
+            this.stringBuilder = new StringBuilder();
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        public override Encoding Encoding => Encoding.Unicode;
+
+        public override void Write(char value)
+        {
+            this.stringBuilder.Append(value);
+        }
+
+        public override void WriteLine(string value)
+        {
+            this.stringBuilder.Append(value);
+
+            this.Flush();
+        }
+
+        public override void Flush()
+        {
+            var sb = this.stringBuilder;
+            if (sb.Length > 0)
+            {
+                this.stringBuilder = new StringBuilder();
+                this.testOutputHelper.WriteLine(sb.ToString());
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.Flush();
+            base.Dispose(disposing);
+        }
+    }
+}
