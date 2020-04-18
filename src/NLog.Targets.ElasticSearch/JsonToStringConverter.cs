b22403d195841836690b406ef7fb9afcc4b54bb4
Rolf Kristensen <sweaty1@hotmail.com>
2020-04-19 00:50:36 +0200

    feat: skip jsonserialize the entire world when meeting deep reflection types

---
+++ src/NLog.Targets.ElasticSearch/JsonToStringConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NLog.Targets.ElasticSearch
+{
+    internal sealed class JsonToStringConverter : JsonConverter
+    {
+        private readonly Type _type;
+
+        /// <inheritdoc />
+        public override bool CanRead { get; } = false;
+
+        public JsonToStringConverter(Type type)
+        {
+            _type = type;
+        }
+
+        /// <inheritdoc />
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value == null)
+            {
+                writer.WriteNull();
+            }
+            else
+            {
+                writer.WriteValue(value.ToString());
+            }
+        }
+
+        /// <inheritdoc />
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotSupportedException("Only serialization is supported");
+        }
+
+        /// <inheritdoc />
+        public override bool CanConvert(Type objectType)
+        {
+            return _type.IsAssignableFrom(objectType);
+        }
+    }
+}
